9014067a185d7c2b4ccc8ac099239007a13dee79
Matheus A. Favero <matheusdeamorim20@gmail.com>
2024-12-30 16:41:12 -0300

    fix: add dotenv

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,11 +1,12 @@
 import { test, expect } from '@playwright/test';
+require("dotenv").config({path: "./.env"});
 
-const TEST_USER = process.env.TEST_USER;
-const TEST_PASSWORD = process.env.TEST_PASSWORD;
-const NEXT_PUBLIC_APP_URL = process.env.NEXT_PUBLIC_APP_URL ?? "https://todo-app-e2e-tests.pages.dev/";
+const TEST_USER = process.env.NEXT_PUBLIC_TEST_USER;
+const TEST_PASSWORD = process.env.NEXT_PUBLIC_TEST_PASSWORD;
+const NEXT_PUBLIC_APP_URL = process.env.NEXT_PUBLIC_APP_URL;
 
 test('Login', async ({ page }) => {
-  await page.goto(NEXT_PUBLIC_APP_URL);
+  await page.goto(`${NEXT_PUBLIC_APP_URL}`);
   await page.getByPlaceholder('Email').click();
   await page.getByPlaceholder('Email').fill(`${TEST_USER}`);
   await page.getByPlaceholder('Password').click();
