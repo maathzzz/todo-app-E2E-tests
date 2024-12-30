dcda9c6a5aa656752ccff17b32afb798168d3e2b
Matheus A. Favero <matheusdeamorim20@gmail.com>
2024-12-30 17:19:40 -0300

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -31,8 +31,8 @@ export default defineConfig({
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
-  
-  timeout: 90_000,
+
+  expect: { timeout: 10_000 },
 
   /* Configure projects for major browsers */
   projects: [
