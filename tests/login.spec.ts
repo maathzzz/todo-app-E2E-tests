ce0141d2234c57a2656823b8ac5f20a9e01a0a43
Matheus A. Favero <matheusdeamorim20@gmail.com>
2024-12-30 15:58:20 -0300

    feat: add env variables in login test

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,10 +1,14 @@
 import { test, expect } from '@playwright/test';
 
+const TEST_USER = process.env.TEST_USER;
+const TEST_PASSWORD = process.env.TEST_PASSWORD;
+const NEXT_PUBLIC_APP_URL = process.env.NEXT_PUBLIC_APP_URL ?? "https://todo-app-e2e-tests.pages.dev/";
+
 test('Login', async ({ page }) => {
-  await page.goto('https://todo-app-e2e-tests.pages.dev/');
+  await page.goto(NEXT_PUBLIC_APP_URL);
   await page.getByPlaceholder('Email').click();
-  await page.getByPlaceholder('Email').fill('user@example.com');
+  await page.getByPlaceholder('Email').fill(`${TEST_USER}`);
   await page.getByPlaceholder('Password').click();
-  await page.getByPlaceholder('Password').fill('!P4ssw0rdstr!');
+  await page.getByPlaceholder('Password').fill(`${TEST_PASSWORD}`);
   await page.getByRole('button', { name: 'Login' }).click();
 });
