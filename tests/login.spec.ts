db7dc24b1b1f76279dfda9c817e61f0217c6788e
Matheus A. Favero <matheusdeamorim20@gmail.com>
2024-12-30 17:11:44 -0300

    test: github secrets in login test

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,15 +1,16 @@
 import { test, expect } from '@playwright/test';
 require("dotenv").config({path: "./.env.local"});
 
-const TEST_USER = process.env.NEXT_PUBLIC_TEST_USER;
-const TEST_PASSWORD = process.env.NEXT_PUBLIC_TEST_PASSWORD;
-const APP_URL = process.env.APP_URL;
+const TEST_USER = process.env.TEST_USER;
+const TEST_PASSWORD = process.env.TEST_PASSWORD;
+const TEST_URL = process.env.TEST_URL;
 
 test('Login', async ({ page }) => {
-  await page.goto(`${APP_URL}`);
+  await page.goto(`${TEST_URL}`);
   await page.getByPlaceholder('Email').click();
   await page.getByPlaceholder('Email').fill(`${TEST_USER}`);
   await page.getByPlaceholder('Password').click();
   await page.getByPlaceholder('Password').fill(`${TEST_PASSWORD}`);
   await page.getByRole('button', { name: 'Login' }).click();
+  await expect(page).toHaveURL(/.*todos/);
 });
