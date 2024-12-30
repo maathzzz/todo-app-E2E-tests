2251e6f4e4723b18aae6eac980d7ceabb7faeffd
Matheus A. Favero <matheusdeamorim20@gmail.com>
2024-12-30 16:52:57 -0300

    Update login.spec.ts

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -3,10 +3,10 @@ require("dotenv").config({path: "./.env.local"});
 
 const TEST_USER = process.env.NEXT_PUBLIC_TEST_USER;
 const TEST_PASSWORD = process.env.NEXT_PUBLIC_TEST_PASSWORD;
-const NEXT_PUBLIC_APP_URL = process.env.NEXT_PUBLIC_APP_URL;
+const APP_URL = process.env.APP_URL;
 
 test('Login', async ({ page }) => {
-  await page.goto(`${NEXT_PUBLIC_APP_URL}`);
+  await page.goto(`${APP_URL}`);
   await page.getByPlaceholder('Email').click();
   await page.getByPlaceholder('Email').fill(`${TEST_USER}`);
   await page.getByPlaceholder('Password').click();
